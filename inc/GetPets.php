d87950fa9a1283c7f96730318cfad5ecff45c38d
Imon <contact@imonweb.co.uk>
2024-01-04 16:13:27 +0000

    building dynamic queries

---
+++ inc/GetPets.php
@@ -0,0 +1,108 @@
+<?php 
+
+class GetPets {
+  function __construct() {
+    global $wpdb;
+    $tablename = $wpdb->prefix . 'pets';
+    // $ourQuery = $wpdb->prepare("SELECT * FROM $tablename WHERE species = %s LIMIT 100", array($_GET['species']));
+
+    // $ourQuery = $wpdb->prepare("SELECT * FROM $tablename LIMIT 100");
+    // $ourQuery = $wpdb->prepare("SELECT * FROM wp_pets WHERE species = %s AND birthyear > %d LIMIT 10", array('hamster', 2018));
+    // $pets = $wpdb->get_results($ourQuery);
+
+    // $this->pets = $wpdb->get_results($ourQuery);
+
+    /*  Make it dynamic */
+    $this->args = $this->getArgs();
+    $this->placeholders = $this->createPlaceholders();
+
+    $query = "SELECT * FROM $tablename ";
+    $query .= $this->createWhereText();
+    $query .= " LIMIT 100";
+    $this->pets = $wpdb->get_results($wpdb->prepare( $query, $this->placeholders ));
+    
+    // var_dump($pets);
+  } // __construct 
+
+   
+  function getArgs() {
+    $temp = array(
+      'favcolor' => sanitize_text_field($_GET['favcolor']),
+      'species' => sanitize_text_field($_GET['species']),
+      'minyear' => sanitize_text_field($_GET['minyear']),
+      'maxyear' => sanitize_text_field($_GET['maxyear']),
+      'minweight' => sanitize_text_field($_GET['minweight']),
+      'maxweight' => sanitize_text_field($_GET['maxweight']),
+      'favhobby' => sanitize_text_field($_GET['favhobby']),
+      'favfood' => sanitize_text_field($_GET['favfood']),
+    );
+
+    return array_filter($temp, function($x){
+      return $x;
+    });
+  } // getArgs()
+   
+  
+
+  /*  updated */
+  /*
+  function getArgs() {
+    $temp = [];
+ 
+    if (isset($_GET['favcolor'])) $temp['favcolor'] = sanitize_text_field($_GET['favcolor']);
+    if (isset($_GET['species'])) $temp['species'] = sanitize_text_field($_GET['species']);
+    if (isset($_GET['minyear'])) $temp['minyear'] = sanitize_text_field($_GET['minyear']);
+    if (isset($_GET['maxyear'])) $temp['maxyear'] = sanitize_text_field($_GET['maxyear']);
+    if (isset($_GET['minweight'])) $temp['minweight'] = sanitize_text_field($_GET['minweight']);
+    if (isset($_GET['maxweight'])) $temp['maxweight'] = sanitize_text_field($_GET['maxweight']);
+    if (isset($_GET['favhobby'])) $temp['favhobby'] = sanitize_text_field($_GET['favhobby']);
+    if (isset($_GET['favfood'])) $temp['favfood'] = sanitize_text_field($_GET['favfood']);
+ 
+    return $temp;
+ 
+  }
+  */
+
+  function createPlaceholders() {
+    return array_map(function($x) {
+      return $x;
+    }, $this->args);
+  } // createPlaceholders()
+
+  function createWhereText() {
+    $whereQuery = "";
+
+    if(count($this->args)) {
+      $whereQuery = "WHERE ";
+    }
+
+    $currentPosition = 0;
+    foreach($this->args as $index => $item) {
+      $whereQuery .= $this->specificQuery($index);
+      if($currentPosition != count($this->args) -1) {
+        $whereQuery .= " AND ";
+      }
+      $currentPosition++;
+    }
+
+    return $whereQuery;
+  } // createWhereText()
+
+  function specificQuery($index) {
+    switch($index) {
+      case "minweight":
+        return "petweight >= %d";
+      case "maxweight":
+        return "petweight <= %d";
+      case "minyear":
+        return "birthyear >= %d";
+      case "maxyear":
+        return "birthyear <= %d";
+      default:
+        return $index . " = %s";
+    }
+  } // specificQuery()
+
+} // class GetPets {}
+
+  
--- inc/template-pets.php
+++ inc/template-pets.php
@@ -1,10 +1,14 @@
 <?php
 
+require_once plugin_dir_path(__FILE__) . 'GetPets.php';
+$getPets = new GetPets();
+
 get_header(); ?>
 
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
-  <div class="page-banner__content container container--narrow">
+  <!-- <div class="page-banner__content container container--narrow"> -->
+  <div class="page-banner__content container">
     <h1 class="page-banner__title">Pet Adoption</h1>
     <div class="page-banner__intro">
       <p>Providing forever homes one search at a time.</p>
@@ -12,19 +16,11 @@ get_header(); ?>
   </div>  
 </div>
 
-<div class="container container--narrow page-section">
+<!-- <div class="container container--narrow page-section"> -->
+<div class="container page-section">
 
   <p>This page took <strong><?php echo timer_stop();?></strong> seconds to prepare. Found <strong>x</strong> results (showing the first x).</p>
-
-  <?php 
-    global $wpdb;
-    $tablename = $wpdb->prefix . 'pets';
-    $ourQuery = $wpdb->prepare("SELECT * FROM $tablename LIMIT 100");
-    // $ourQuery = $wpdb->prepare("SELECT * FROM wp_pets WHERE species = %s AND birthyear > %d LIMIT 10", array('hamster', 2018));
-    $pets = $wpdb->get_results($ourQuery);
-    // var_dump($pets);
-  ?>
-
+ 
   <table class="pet-adoption-table">
     <tr>
       <th>Name</th>
@@ -36,7 +32,7 @@ get_header(); ?>
       <th>Favorite Food</th>
     </tr>
     <?php 
-      foreach($pets as $pet){ ?>
+      foreach($getPets->pets as $pet){ ?>
         <tr>
           <td><?php echo $pet->petname; ?></td>
           <td><?php echo $pet->species; ?></td>
